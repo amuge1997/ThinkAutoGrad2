3da71ceaa834e47bc1a03560ef3446ff669106da
“amuge1997” <amuge1997z@qq.com>
2022-02-02 23:41:38 +0800

    删除__pycache__;

--- ThinkAutoGrad2/Model.py
+++ ThinkAutoGrad2/Model.py
@@ -1,3 +1,5 @@
+from ThinkAutoGrad2.Tensor import Tensor
+import pickle
 
 
 class Model:
@@ -10,12 +12,37 @@ class Model:
     def forward(self, inps):
         pass
 
+    def append_weights(self, *weights):
+        for w in weights:
+            if type(w) != Tensor:
+                raise Exception('非 Tensor 类型')
+            self.weights_list.append(w)
+
     def get_weights(self):
         return self.weights_list
 
     def grad_zeros(self):
         for w in self.weights_list:
             w.grad_zeros()
+
+    def save_weights(self, path):
+        ws = self.weights_list
+        with open(path, 'wb') as fp:
+            pickle.dump(ws, fp)
+
+    def load_weights(self, path):
+        with open(path, 'rb') as fp:
+            ws = pickle.load(fp)
+        for w in ws:
+            if type(w) != Tensor:
+                raise Exception('非 Tensor 类型')
+        self.weights_list = ws
+
+
+
+
+
+
 
 
 
